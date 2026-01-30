c386886183d2c47d92d7336e844980e61ffece37
Ivan <ivandaza2004@gmail.com>
2026-01-29 23:52:20 -0300

    Logo C

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/c.svg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/c.svg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/c.svg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/c.svg',
   },
 }
 
